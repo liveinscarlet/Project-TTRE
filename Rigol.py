dd3578e3a48290d3eecc726c2d2785db6939f26c
liveinscarlet <venya065@mail.ru>
2022-11-04 23:30:08 +0300

    Script for receiving data from the oscilloscope

--- Rigol.py
+++ Rigol.py
@@ -1,4 +1,3 @@
-import visa
 from numpy import arrange
 import pyvisa
 import matplotlib as plt
@@ -18,41 +17,40 @@ class oscil(object):
         myOsc = rm.open_resource("TCPIP0::192.168.1.10::inst0::INSTR") #Для корректной работы надо отключать защиту осцила
     print(myOsc.query("*IDN?"))
     def dataExtraction (self):
-        oscil.myOsc.querry (":WAVeform:DATA?")
+        oscil.myOsc.query (":WAVeform:DATA?")
 
     def DefSetup (self):
-        oscil.myOsc.querry (":WAVEFORM:SOURCE CHANNEL1")
-        oscil.myOsc.querry (":WAVEFORM:FORMAT WORD")
-        oscil.myOsc.querry (":SYSTEM:HEADER OFF")
+        oscil.myOsc.query (":WAVEFORM:SOURCE CHANNEL1")
+        oscil.myOsc.query (":WAVEFORM:FORMAT WORD")
+        oscil.myOsc.query (":SYSTEM:HEADER OFF")
         return waveform
     pass
-
 
 class PURigol (object):
     if __name__ == '__main__':
         rm = pyvisa.ResourceManager()
         myPU = rm.open_resource("TCPIP0::192.168.1.10::inst0::INSTR") #Узнать IP БП и поменять код соответственно
-    print (myPU.querry("*IDN?"))
+    print (myPU.query("*IDN?"))
 
     def Deafult_Setup(self):
-        PURigol.myPU.querry(":CH1 :CURR:PROT 0.1")
-        PURigol.myPU.querry(":CH2 :CURR:PROT 0.1")
-        PURigol.myPU.querry(":APPL CH1,0,0")
-        PURigol.myPU.querry(":APPL CH2,0,0")
+        PURigol.myPU.query(":CH1 :CURR:PROT 0.1")
+        PURigol.myPU.query(":CH2 :CURR:PROT 0.1")
+        PURigol.myPU.query(":APPL CH1,0,0")
+        PURigol.myPU.query(":APPL CH2,0,0")
 
     def VoltagesChange (self):
         i, j = 0, 0
         for i in V_plus:
             for j in V_minus:
-                PURigol.myPU.querry ("APPL CH2, j")
+                PURigol.myPU.query ("APPL CH2, j")
                 j+=step
             j=0
             i+=step
-            PURigol.myPU.querry("APPL CH1, i")
+            PURigol.myPU.query("APPL CH1, i")
 
     def SetVolt (self, volt_plus, volt_minus):
-        PURigol.myPU.querry(":APPL CH1, volt_plus")
-        PURigol.myPU.querry(":APPL CH2, volt_minus")
+        PURigol.myPU.query(":APPL CH1, volt_plus")
+        PURigol.myPU.query(":APPL CH2, volt_minus")
 
     pass
 
