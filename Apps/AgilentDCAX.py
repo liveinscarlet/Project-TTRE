36bd1ef61de1ef74eca7f0c0225157ab7f526471
liveinscarlet <venya065@mail.ru>
2022-11-10 23:30:20 +0300

    Data extraction from the oscilloscope

--- Apps/AgilentDCAX.py
+++ Apps/AgilentDCAX.py
@@ -5,7 +5,6 @@ import time
 import numpy as np
 import numpy.typing as npt
 
-time = []
 ampl = []
 i = 0
 
@@ -16,7 +15,10 @@ class OscilloscopeAgilent86100D(object):
         self.myOsc = visa_manager.open_resource(addr)  # Для корректной работы надо отключать защиту осцила
         print(self.myOsc.query("*IDN?"))
 
-    def dataExtraction(self) -> npt.NDArray[float]:
+    def Preamble(self):
+        return self.myOsc.query(":WAV:PRE?")
+
+    def dataExtraction(self):
         return self.myOsc.query(":WAVeform:DATA?")
 
     def AmpWidth(self):
@@ -35,22 +37,26 @@ class OscilloscopeAgilent86100D(object):
         # self.myOsc.write(":WAVEFORM:FORMAT BYTE")
         self.myOsc.write(":SYSTEM:HEADER OFF")
         self.myOsc.write(":CHANNEL1:OFFSET 0")
+        self.myOsc.write(":WAVEFORM:FORMAT ASCII")
+        self.myOsc.write(":WAVEFORM:XYFORMAT")
         # self.myOsc.write(":CHANNEL1:PRlOBE 46 DEC")  # Channel 1 attenuation 46 dB
 
 if __name__ == '__main__':
     rm = pyvisa.ResourceManager()
     Agil = OscilloscopeAgilent86100D(rm, 'TCPIP0::192.168.1.5::inst0::INSTR')
     Agil.DefSetup()
+    preabmle = Agil.Preamble()
+    preabmle = preabmle.split(sep=',')
+    points = int(preabmle[2])
+    start_time = float(preabmle[12])
+    time_range = float(preabmle[11])
+    time_end = start_time+time_range
+    time = np.linspace(start_time, time_end, points)
     waveform = Agil.dataExtraction()
     waveform = waveform.split(sep=',')
-    # waveform = [float(x) for x in waveform]
-    while i < len(waveform):
-        if i % 2 == 0:
-            time.append(waveform[i])
-        else:
-            ampl.append(waveform[i])
-        i += 1
+    waveform = [float(x) for x in waveform]
 
-plt.plot(waveform)
-plt.show()
-plt.grid(True)
+    plt.plot(time, waveform)
+    plt.show()
+    plt.grid(True)
+    plt.ylim(-1, 1)
