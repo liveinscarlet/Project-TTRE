1d38e0ffa67cd293dc55a1a3ed8de3bfb3053a11
liveinscarlet <venya065@mail.ru>
2022-11-12 11:41:03 +0300

    Update

--- Apps/DataProcessing.py
+++ Apps/DataProcessing.py
@@ -2,6 +2,9 @@ import numpy
 import matplotlib as plt
 
 class Plots(object):
-    def maps(self):
-        fig = plt.figure(figsize=(9, 3))
+    def maps(self, x, y, z):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(x, y, z, cmap='inferno')
+        ax.legend()
         plt.show()
--- Apps/Experiment.py
+++ Apps/Experiment.py
@@ -5,9 +5,6 @@ import time
 from AgilentDCAX import OscilloscopeAgilent86100D
 from Rigol import PURigol
 
-V_start, V_max = 0, 10
-step = 0.5
-Voltages = arange(V_start, V_max, step)  # Array of positive voltages
 waveform = []  # Signal from the oscilloscope
 time_dom = []  # x axes
 amplitude = []  # y axes
@@ -15,7 +12,7 @@ max_amp = []  # Meanings of the amplitudes of the pulses
 pulse_width = []  # Array of pulses width
 
 
-class Experiment(PURigol, OscilloscopeAgilent86100D):
+class Experiment(object):
     def __int__(self):
         rm = pyvisa.ResourceManager()
         Rigol = PURigol(rm, 'TCPIP0::192.168.1.227::inst0::INSTR')  # The name of the PU in the experiment
@@ -25,28 +22,28 @@ class Experiment(PURigol, OscilloscopeAgilent86100D):
         Rigol.Deafult_Setup_CH2()
         Agilent.DefSetup()
 
-    def ampl_width_map(self, v_min, v_max, step):
+    def ampl_width_map(self):
         v_min = float(input('Voltage to begin'))
         v_max = float(input('Maximum voltage'))
         step = float(input('Step for voltages'))
         ampl = []
+        time_mid = []
         voltages = arange(v_min, v_max, step)
-        for i in Voltages:
+        for i in voltages:
             PURigol.Voltage_Change_CH1(i)
-            for j in Voltages:
+            for j in voltages:
                 PURigol.Voltage_Change_CH2(j)
                 values = OscilloscopeAgilent86100D.GetYData()
-                ampl.append(max(values))
+                ampl[i j] = max(values)
                 middle = 0.5*max(values)
                 k = 0
-                time_mid = []
                 while k < len(values):
                     if values(k)<middle:
                         continue
                     else:
                         time_mid.append(values[k])
-                width = time_mid[-1] - time_mid[0]
-                pulse_width.append(width)
+                width_pulse = time_mid[-1] - time_mid[0]
+                pulse_width[i j] = width_pulse
                 pass
         return (voltages, ampl, width)
 
@@ -65,5 +62,12 @@ class Experiment(PURigol, OscilloscopeAgilent86100D):
 
     # def neg_pulse_end(waveform):
     #     return neg_time_end
-
-# if __name__ == "__main__":
+    #
+if __name__ == "__main__":
+    rm = pyvisa.ResourceManager()
+    Rigol = PURigol(rm, 'TCPIP0::192.168.1.227::inst0::INSTR')  # The name of the PU in the experiment
+    Rigol.Deafult_Setup_CH1()
+    Rigol.Deafult_Setup_CH2()
+    Agilent = OscilloscopeAgilent86100D(rm, 'TCPIP0::192.168.1.5::inst0::INSTR')  # The name of the osc in the exp
+    exp = Experiment()
+    voltages, ampl, width = exp.ampl_width_map()
